ac81bbf3a97b5468dacce91cb8b4b47dde3cbf66
lutfullahillabib <lutfullahillabib@gmail.com>
2022-11-08 01:12:49 +0600

    basic index.js & data uploaded on mongo

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+
+// const jwt = require('jsonwebtoken');
+
+// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+// require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle-wares
+app.use(cors());
+app.use(express.json());
+
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASSWORD);
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jjkmnej.mongodb.net/?retryWrites=true&w=majority`;
+
+// console.log(uri);
+
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.get('/', (req, res) => {
+    res.send('Photography-with-Labib server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Photography-with-Labib server running on ${port}`);
+})
