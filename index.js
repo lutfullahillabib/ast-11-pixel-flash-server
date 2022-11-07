75f8b72dd4a3267f30f23988bae90d97df4e7afc
lutfullahillabib <lutfullahillabib@gmail.com>
2022-11-08 01:17:22 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 // const jwt = require('jsonwebtoken');
 
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // require('dotenv').config();
 
@@ -17,11 +17,11 @@ app.use(express.json());
 // console.log(process.env.DB_USER);
 // console.log(process.env.DB_PASSWORD);
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jjkmnej.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jjkmnej.mongodb.net/?retryWrites=true&w=majority`;
 
-// console.log(uri);
+console.log(uri);
 
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 app.get('/', (req, res) => {
@@ -31,3 +31,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Photography-with-Labib server running on ${port}`);
 })
+
+
