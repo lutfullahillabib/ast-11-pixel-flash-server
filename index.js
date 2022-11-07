2bd903830e1988a74b9692d706e0fde8e48f5597
lutfullahillabib <lutfullahillabib@gmail.com>
2022-11-08 01:20:55 +0600

    jwt token

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-// require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,8 +14,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// console.log(process.env.DB_USER);
-// console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jjkmnej.mongodb.net/?retryWrites=true&w=majority`;
 
